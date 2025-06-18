3a1b6a7b7723b5e7f85f7fcdb49904478d6bb9d4
binh181106 <ad.ntb.ntb@gmail.com>
2025-06-19 00:07:54 +0700

    Download django-configurations on MSI

--- manage.py
+++ manage.py
@@ -2,12 +2,16 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from configurations.management import execute_from_command_line
+
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blango.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blango.settings")
     os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
+
+
     try:
         from configurations.management import execute_from_command_line
     except ImportError as exc:
